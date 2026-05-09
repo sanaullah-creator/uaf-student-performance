03a3b7279a56681da76acbab852470a939b2f7e0
sanaullah-creator <lsanau860@gmail.com>
2026-05-09 11:07:42 +0500

    Added openpyxl dependency

--- app.py
+++ app.py
@@ -738,9 +738,8 @@ def export_excel():
     )
 
 
-# ====================== MAIN ======================
-if __name__ == '__main__':
+import os
 
-    print("🚀 Server Running → http://127.0.0.1:5000")
-
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
